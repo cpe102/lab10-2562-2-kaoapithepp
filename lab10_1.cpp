6bc01b600ffcbfea5a41b959c2f445acd17d57fd
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-31 15:33:58 +0700

    soEasy

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -16,16 +16,16 @@ int main(){
 
     srand(time(0));
     int x = rand()%9;
-    if (x == 0) cout << "You will get " << grade[0] << " in this 261102" << endl;
-    else if (x == 1) cout << "You will get " << grade[1] << " in this 261102" << endl;
-    else if (x == 2) cout << "You will get " << grade[2] << " in this 261102" << endl;
-    else if (x == 3) cout << "You will get " << grade[3] << " in this 261102" << endl;
-    else if (x == 4) cout << "You will get " << grade[4] << " in this 261102" << endl;
-    else if (x == 5) cout << "You will get " << grade[5] << " in this 261102" << endl;
-    else if (x == 6) cout << "You will get " << grade[6] << " in this 261102" << endl;
-    else if (x == 7) cout << "You will get " << grade[7] << " in this 261102" << endl;
-    else if (x == 8) cout << "You will get " << grade[8] << " in this 261102" << endl;
-    else if (x == 9) cout << "You will get " << grade[9] << " in this 261102" << endl;
+    if (x == 0) cout << "You will get " << grade[0] << " in this 261102." << endl;
+    else if (x == 1) cout << "You will get " << grade[1] << " in this 261102." << endl;
+    else if (x == 2) cout << "You will get " << grade[2] << " in this 261102." << endl;
+    else if (x == 3) cout << "You will get " << grade[3] << " in this 261102." << endl;
+    else if (x == 4) cout << "You will get " << grade[4] << " in this 261102." << endl;
+    else if (x == 5) cout << "You will get " << grade[5] << " in this 261102." << endl;
+    else if (x == 6) cout << "You will get " << grade[6] << " in this 261102." << endl;
+    else if (x == 7) cout << "You will get " << grade[7] << " in this 261102." << endl;
+    else if (x == 8) cout << "You will get " << grade[8] << " in this 261102." << endl;
+    else if (x == 9) cout << "You will get " << grade[9] << " in this 261102." << endl;
 
     return 0;
     
